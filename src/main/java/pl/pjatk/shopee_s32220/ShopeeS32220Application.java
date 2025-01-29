70214d7bf2211db689e4d99ab945579314fcacb3
ax8tje <131400863+ax8tje@users.noreply.github.com>
2025-01-29 17:17:09 +0100

    1.0

---
+++ src/main/java/pl/pjatk/shopee_s32220/ShopeeS32220Application.java
@@ -0,0 +1,13 @@
+package pl.pjatk.shopee_s32220;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopeeS32220Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopeeS32220Application.class, args);
+	}
+
+}
---
+++ src/test/java/pl/pjatk/shopee_s32220/ShopeeS32220ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pjatk.shopee_s32220;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopeeS32220ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
